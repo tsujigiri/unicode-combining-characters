e9e7b274886cf013f41aed3e2efd1ac442f5d6b8
Helge Rausch <helge@rausch.io>
2015-08-12 09:12:21 +0200

    Add type annotations

--- src/UnicodeCombiningCharacters.hs
+++ src/UnicodeCombiningCharacters.hs
@@ -1,9 +1,15 @@
-module UnicodeCombiningCharacters where
+module UnicodeCombiningCharacters (
+    strike,
+    underline
+    ) where
 
+strike :: String -> String
 strike = combine '\822'
 
+underline :: String -> String
 underline = combine '\818'
 
+combine :: Char -> String -> String
 combine combiningChar string = do
     char <- string
     [char, combiningChar]
